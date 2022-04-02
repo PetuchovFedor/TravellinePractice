2f9acb6a8753f6872a7588fded01bbca2a2ab06f
PetuchovFedor <petuchov.fedya.a.eto.ya@gmail.com>
2022-04-02 17:28:17 +0300

    Обновил frontend

--- frontend/todo-list/src/app/app.component.ts
+++ frontend/todo-list/src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
+import { discardPeriodicTasks } from '@angular/core/testing';
 import { NgForm } from '@angular/forms';
+import {Task} from './item/ITask'
+// import { count } from 'console';
+// import { takeLast } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,26 +12,36 @@ import { NgForm } from '@angular/forms';
 })
 
 export class AppComponent {
-  name: string = "Fedya";
-  showName: boolean = true;
- 
-  applesNumber:number =10;
-  addApple(): void {
-    this.applesNumber++;
+  showCompletedTasks: boolean = true;
+  show(): void{
+    if (this.showCompletedTasks){
+      this.showCompletedTasks = false;
+    } else {
+      this.showCompletedTasks = true;
+    }
   }
   
-  tasks:Array<string> = [
-   "go to cinema", "go to shop"
+  tasks:Array<Task> = [
+    {id: 1, name: "Проснуться в 6:00"}, 
+    {id: 2, name: "Позавтракать"},
+    {id: 3, name: "Умыться"}
   ]
-  addTask(myForm:NgForm):void {
-     this.tasks.push(myForm.value.task);
-  }
-  onDelete(text:string): void {
-    this.tasks = this.tasks.filter(task => task != text)
-  }  
-}
 
-export interface task{
-  id: number;
-  name: string;
+  completedTasks: Array<Task> =[];
+
+  taskId = this.tasks.length + 1;
+  addTask(myForm:NgForm):void {
+    this.tasks.push({id: this.taskId, name: myForm.value.task});
+    this.taskId++;
+  }
+
+  onDelete(id:number): void {
+    this.tasks = this.tasks.filter(task => task.id != id)
+  }
+ 
+  onComplete(id:number):void {
+    let completedIndex = this.tasks.findIndex(task => task.id == id);
+    this.completedTasks.push(this.tasks[completedIndex]);
+    this.tasks = this.tasks.filter(task => task.id != id);
+  }
 }
--- frontend/todo-list/src/app/item/item.component.ts
+++ frontend/todo-list/src/app/item/item.component.ts
@@ -1,15 +1,20 @@
 import { Component, Input, EventEmitter, Output } from '@angular/core';
-
+import {Task} from './ITask'
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.css']
 })
+
 export class ItemComponent {
-  @Input() text?:string;
+  @Input() task!: Task;
   @Output() onDeleteTask = new EventEmitter();
+  @Output() onCompleteTask = new EventEmitter();
   delete():void {
-    this.onDeleteTask.emit(this.text as string)
+    this.onDeleteTask.emit(this.task.id)
+  }
+  complete():void {
+    this.onCompleteTask.emit(this.task.id)
   }
 }
 
